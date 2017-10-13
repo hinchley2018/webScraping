3fe8ccfb86c66bcc00952c084d45bd9fdc8c48ca
Jonathon Hinchley <jhinchley@Jonathons-MacBook-Air.local>
2017-10-13 08:55:12 -0500

    learned enough about bs to be dangerous

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import requests
+from bs4 import BeautifulSoup
+def main():
+    page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+    print page
+    if (page.status_code == 200):
+        print page.content
+        soup = BeautifulSoup(page.content,'html.parser')
+        soup.find_all('p')
+if __name__ == '__main__':
+    main()
